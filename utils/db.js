67646ceb7a224019567467e37417de4877a56ed0
Brian <chumobrian@gmail.com>
2024-06-19 10:49:20 +0300

    feat: Add a worker.js file to  add thhumbnails to a queue

--- utils/db.js
+++ utils/db.js
@@ -1,64 +1,51 @@
-// create a file db.js that contains the class DBClient.
-
-// DBClient should have:
-
-// the constructor that creates a client to MongoDB:
-// host: from the environment variable DB_HOST or default: localhost
-// port: from the environment variable DB_PORT or default: 27017
-// database: from the environment variable DB_DATABASE or default: files_manager
-// a function isAlive that returns true when the connection to MongoDB is a success otherwise, false
-// an asynchronous function nbUsers that returns the number of documents in the collection users
-// an asynchronous function nbFiles that returns the number of documents in the collection files
-// After the class definition, create and export an instance of DBClient called dbClient.
-
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 class DBClient {
-    constructor() {
-        // Creates a client to MongoDB with host, port and database names from environment variables or otherwise default
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    // Creates a client to MongoDB with host, port and database names from environment variables or otherwise default
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-        const url = `mongodb://${host}:${port}`;
+    const url = `mongodb://${host}:${port}`;
 
-        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.db = null;
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err);
+      });
+  }
 
-        this.client.connect()
-        .then(() => {
-            this.db = this.client.db(database);
-        })
-        .catch((err) => {
-            console.error("Failed to connect to MongoDB:", err)
-        });
+  isAlive() {
+    // Returns true when connection to MongoD is  success otherwise false
+    return this.client && this.client.isConnected() && this.db;
+  }
+
+  async nbUsers() {
+    // Returns number of documents in collection users, otherwise returns 0 incase of an error
+    try {
+      return await this.db.collection('users').countDocuments();
+    } catch (err) {
+      console.error('Error counting users:', err);
+      return 0;
     }
+  }
 
-    isAlive() {
-        //Returns true when connection to MongoD is  success otherwise false
-        return this.client && this.client.isConnected() && this.db !== undefined;
+  async nbFiles() {
+    // returns number of documents in collection files, otherwise 0 incase of an error
+    try {
+      return await this.db.collection('files').countDocuments();
+    } catch (err) {
+      console.error('Error counting Files:', err);
+      return 0;
     }
-
-    async nbUsers() {
-        // Returns number of documents in collection users, otherwise returns 0 incase of an error
-        try {
-            return await this.db.collection('users').countDocuments();
-        } catch (err) {
-            console.error("Error counting users:", err);
-            return 0;
-        }
-    }
-
-    async nbFiles() {
-        // returns number of documents in collection files, otherwise 0 incase of an error
-        try {
-            return await this.db.collection('files').countDocuments();
-        } catch (err) {
-            console.error('Error counting Files:', err);
-            return 0;
-        }
-    }
+  }
 }
 
-const dbClient = new DBClient;
-module.exports = dbClient; 
+const dbClient = new DBClient();
+module.exports = dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -1,69 +1,56 @@
-/*
-Inside the folder utils, create a file redis.js that contains the class RedisClient.
-
-RedisClient should have:
-
-the constructor that creates a client to Redis:
-any error of the redis client must be displayed in the console (you should use on('error') of the redis client)
-a function isAlive that returns true when the connection to Redis is a success otherwise, false
-an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
-an asynchronous function set that takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
-an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
-After the class definition, create and export an instance of RedisClient called redisClient.
-*/
-
 const redis = require('redis');
 const util = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => {
-            console.log(`Redis client not connected to the server: ${err}`);
-         });
-        this.client.on('connect', () => {
-             console.log('Redis client connected to the server');
-         })
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err}`);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
 
-         this.getAsync = util.promisify(this.client.get).bind(this.client);
-         this.setAsync = util.promisify(this.client.set).bind(this.client);
-         this.delAsync = util.promisify(this.client.del).bind(this.client);
-    }
-    isAlive () {
-        this.client;
-         return this.client.connected;
-    }
+    this.getAsync = util.promisify(this.client.get).bind(this.client);
+    this.setAsync = util.promisify(this.client.set).bind(this.client);
+    this.delAsync = util.promisify(this.client.del).bind(this.client);
+  }
 
-    async get (key) {
-        //converts a function to an async function
-        //const getAsync = util.promisify(this.client.get).bind(this.client);
+  isAlive() {
+    // return this.client;
+    return this.client.connected;
+  }
 
-        //returns a stored value for the key in the redis server
-        try {
-            return await this.getAsync(key);
-        } catch(err) {
-            console.error("Error getting key from Redis:", err);
-            return null;
-        };
-    }
+  async get(key) {
+    // converts a function to an async function
+    // const getAsync = util.promisify(this.client.get).bind(this.client);
 
-    async set (key, value, time) {
-        /*stores elements in the redis server*/
-        try{
-            await this.setAsync(key, value, 'EX', time);
-        } catch(err) {
-            console.error("Error setting key in Redis:", err);
-        }
+    // returns a stored value for the key in the redis server
+    try {
+      return await this.getAsync(key);
+    } catch (err) {
+      console.error('Error getting key from Redis:', err);
+      return null;
     }
+  }
 
-    async del (key) {
-        /*Deletes a key in the redis server*/
-        try {
-            await this.delAsync(key);
-        } catch(err) {
-            console.error("Error deleting key in Redis:", err);
-        }
+  async set(key, value, time) {
+    /* stores elements in the redis server */
+    try {
+      await this.setAsync(key, value, 'EX', time);
+    } catch (err) {
+      console.error('Error setting key in Redis:', err);
     }
+  }
+
+  async del(key) {
+    /* Deletes a key in the redis server */
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.error('Error deleting key in Redis:', err);
+    }
+  }
 }
 
 const redisClient = new RedisClient();
