69fddf4f2e61310004cb3baa0b313b42bed17e11
Brian <chumobrian@gmail.com>
2024-06-19 12:42:31 +0300

    fix: fix error  This line has a length of 117. Maximum allowed is 100  max-len

--- utils/db.js
+++ utils/db.js
@@ -1,50 +1,31 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-class DBClient {
+class MongoDB {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
 
-    const url = `mongodb://${host}:${port}`;
-
+    const url = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.db = null;
-    this.client.connect()
-      .then(() => {
-        this.db = this.client.db(database);
-      })
-      .catch((err) => {
-        console.error('Failed to connect to MongoDB:', err);
-      });
+    this.client.connect();
   }
 
   isAlive() {
-    // Returns true when connection to MongoD is  success otherwise false
-    return this.client && this.client.isConnected() && this.db;
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    // Returns number of documents in collection users, otherwise returns 0 incase of an error
-    try {
-      return await this.db.collection('users').countDocuments();
-    } catch (err) {
-      console.error('Error counting users:', err);
-      return 0;
-    }
+    const count = await this.client.db().collection('users').countDocuments();
+    return count;
   }
 
   async nbFiles() {
-    // returns number of documents in collection files, otherwise 0 incase of an error
-    try {
-      return await this.db.collection('files').countDocuments();
-    } catch (err) {
-      console.error('Error counting Files:', err);
-      return 0;
-    }
+    const count = await this.client.db().collection('files').countDocuments();
+    return count;
   }
 }
 
-const dbClient = new DBClient();
+const dbClient = new MongoDB();
 module.exports = dbClient;
