6c6de49db32171051462faa2ad3df5d2d3d19948
Brian <chumobrian@gmail.com>
2024-06-21 14:05:25 +0300

    fix: remove a dotenv import statement

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
